3d6cd6634585fd8d7f6def1315b83d36a7b876b4
svitlanabradley <svitlana.bradley@gmail.com>
2025-09-02 16:46:26 +0200

    first commit

--- server.js
+++ server.js
@@ -6,25 +6,40 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
 
+// Index rout
+app.get("/", function (req, res) {
+  res.render("index", {title: "Home"})
+})
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 const host = process.env.HOST
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`app listening on ${port}`)
 })
