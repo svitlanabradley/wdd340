7ee1811e08e7748ba69bd778efb1b78995effe56
svitlanabradley <svitlana.bradley@gmail.com>
2025-09-16 15:47:15 +0200

    added content delivery

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 /* ***********************
  * Require Statements
  *************************/
+const inventoryRoute = require("./routes/inventoryRoute")
+const baseController = require("./controllers/baseController")
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
@@ -23,12 +25,13 @@ app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Routes
  *************************/
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
 app.use(static)
 
 // Index rout
-app.get("/", function (req, res) {
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
 
 /* ***********************
  * Local Server Information
