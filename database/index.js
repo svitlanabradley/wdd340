02216464f1400616b1e3e1dda10b868c30ee499b
svitlanabradley <svitlana.bradley@gmail.com>
2025-10-12 22:36:51 +0200

    modifyed

--- database/index.js
+++ database/index.js
@@ -1,37 +1,71 @@
+// const { Pool } = require("pg")
+// require("dotenv").config()
+// /* ***************
+//  * Connection Pool
+//  * SSL Object needed for local testing of app
+//  * But will cause problems in production environment
+//  * If - else will make determination which to use
+//  * *************** */
+// let pool
+// if (process.env.NODE_ENV == "development") {
+//   pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+// })
+
+// // Added for troubleshooting queries
+// // during development
+// module.exports = {
+//   async query(text, params) {
+//     try {
+//       const res = await pool.query(text, params)
+//       console.log("executed query", { text })
+//       return res
+//     } catch (error) {
+//       console.error("error in query", { text })
+//       throw error
+//     }
+//   },
+// }
+// } else {
+//   pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//   })
+//   module.exports = pool
+// }
+
+
 const { Pool } = require("pg")
 require("dotenv").config()
-/* ***************
- * Connection Pool
- * SSL Object needed for local testing of app
- * But will cause problems in production environment
- * If - else will make determination which to use
- * *************** */
-let pool
-if (process.env.NODE_ENV == "development") {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-})
 
-// Added for troubleshooting queries
-// during development
-module.exports = {
-  async query(text, params) {
-    try {
-      const res = await pool.query(text, params)
-      console.log("executed query", { text })
-      return res
-    } catch (error) {
-      console.error("error in query", { text })
-      throw error
-    }
-  },
-}
-} else {
+let pool
+
+if (process.env.NODE_ENV === "development") {
+  // Local dev: allow self-signed certs
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
   })
-  module.exports = pool
+} else {
+  // Production: SSL required
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }, // Render Postgres requires SSL
+  })
 }
+
+// Unified query function
+async function query(text, params) {
+  try {
+    const res = await pool.query(text, params)
+    console.log("executed query", { text })
+    return res
+  } catch (error) {
+    console.error("error in query", { text, error: error.message })
+    throw error
+  }
+}
+
+module.exports = { query, pool }
